fbbc03b2cb2712d54c1eeefa8da47d7410f91d2d
Isaac de Moraes <30375595+Iakim@users.noreply.github.com>
2020-01-17 16:44:17 -0400

    Update robots.sh

--- robots.sh
+++ robots.sh
@@ -10,8 +10,8 @@ ano=`date +%Y`
 mes=`date +%b`
 dia=`date +%e`
 mesn=`date +%m`
+logs='/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX'
 under='_nginx'
-pasta="/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX"
 
 if [ $dia -gt "10" ]
 then
@@ -21,13 +21,13 @@ else
         diaant=`echo $zero$(($dia-1))`
 fi
 
-ips=`sort "$pasta/$ano$mesn$diaant/lista.txt"`
+ips=`sort "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mesn$diaant/lista.txt"`
 
 for hora in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23;
 do
-	comando="sort '$pasta/$ano$mesn$diaant/access_portal_log-$ano$mesn$diaant$under' | grep $ips | grep $diaant/$mes/$ano:$hora | awk '{print\$1}' | uniq -c | sort -n -t: -k1 > '$pasta/$ano$mesn$diaant/$diaant-$mesn-$ano-$hora'"
-	echo "$comando" > "$pasta"/$ano$mesn$diaant/comando.sh
-	sh "$pasta"/$ano$mesn$diaant/comando.sh
+	comando="sort '$logs/$ano$mesn$diaant/access_portal_log-$ano$mesn$diaant$under' | grep $ips | grep $diaant/$mes/$ano:$hora | awk '{print\$1}' | uniq -c | sort -n -t: -k1 > '$logs/$ano$mesn$diaant/$diaant-$mesn-$ano-$hora'"
+	echo "$comando" > comando.sh
+	sh comando.sh
 done
-rm -rf "$pasta"/$ano$mesn$diaant/comando.sh
-sh "$pasta"/create_csv.sh
+rm -rf comando.sh
+sh create_csv.sh
