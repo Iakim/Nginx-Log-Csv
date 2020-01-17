e286971ca225814036f9e5d9396e595c36be53f1
Isaac de Moraes <30375595+Iakim@users.noreply.github.com>
2020-01-17 10:38:29 -0400

    Update nginx_toptwenty.sh

--- nginx_toptwenty.sh
+++ nginx_toptwenty.sh
@@ -9,6 +9,7 @@
 ano=`date +%Y`
 mes=`date +%m`
 dia=`date +%e`
+pasta="/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX"
 
 if [ $dia -gt "10" ]
 then
@@ -18,33 +19,33 @@ else
         diaant=`echo $zero$(($dia-1))`
 fi
 
-mkdir -p "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant"
+mkdir -p "$pasta/$ano$mes$diaant"
 
 for nginx in 15 16;
 do
-	scp root@sinvp-0$nginx.in.local:/var/log/nginx/access_portal_log-"$ano""$mes""$diaant"_sinvp-0$nginx "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant"
+	scp root@sinvp-0$nginx.in.local:/var/log/nginx/access_portal_log-"$ano""$mes""$diaant"_sinvp-0$nginx "$pasta/$ano$mes$diaant"
 done
 
-cat "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_sinvp-015" >> "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_sinvp-016"
-mv "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_sinvp-016" "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_nginx"
+cat "$pasta/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_sinvp-015" >> "$pasta/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_sinvp-016"
+mv "$pasta/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_sinvp-016" "$pasta/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_nginx"
 
-sort "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_nginx" | awk '{print$1}' | uniq -c | sort -n -t: -k1 | tail -n20 > "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/"$ano""$mes""$diaant"_nginx.txt"
-sort "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/"$ano""$mes""$diaant"_nginx.txt" | awk '{print$2}' >> "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista0.txt"
+sort "$pasta/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_nginx" | awk '{print$1}' | uniq -c | sort -n -t: -k1 | tail -n20 > "$pasta/$ano$mes$diaant/"$ano""$mes""$diaant"_nginx.txt"
+sort "$pasta/$ano$mes$diaant/"$ano""$mes""$diaant"_nginx.txt" | awk '{print$2}' >> "$pasta/$ano$mes$diaant/lista0.txt"
 
-sort "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista0.txt" | uniq -c > "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista00.txt"
-cat "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista00.txt" | awk '{print$2}' >> "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista1.txt"
-cp "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista1.txt" "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/ips1.txt"
-cat "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/ips1.txt" | awk 'BEGIN { ORS = " " } { print }' > "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/ips.txt"
-sed -i 's/$/ \\| &/' "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista1.txt" 
-cat "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista1.txt" | awk 'BEGIN { ORS = " " } { print }' > "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista.txt"
-sed -i -r 's/\s+//g' "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista.txt"
-sed -i 's/.\{2\}$//' "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista.txt"
-sed -i "s/$/'&/" "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista.txt"
-sed -i "s/^/'/" "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista.txt"
+sort "$pasta/$ano$mes$diaant/lista0.txt" | uniq -c > "$pasta/$ano$mes$diaant/lista00.txt"
+cat "$pasta/$ano$mes$diaant/lista00.txt" | awk '{print$2}' >> "$pasta/$ano$mes$diaant/lista1.txt"
+cp "$pasta/$ano$mes$diaant/lista1.txt" "$pasta/$ano$mes$diaant/ips1.txt"
+cat "$pasta/$ano$mes$diaant/ips1.txt" | awk 'BEGIN { ORS = " " } { print }' > "$pasta/$ano$mes$diaant/ips.txt"
+sed -i 's/$/ \\| &/' "$pasta/$ano$mes$diaant/lista1.txt" 
+cat "$pasta/$ano$mes$diaant/lista1.txt" | awk 'BEGIN { ORS = " " } { print }' > "$pasta/$ano$mes$diaant/lista.txt"
+sed -i -r 's/\s+//g' "$pasta/$ano$mes$diaant/lista.txt"
+sed -i 's/.\{2\}$//' "$pasta/$ano$mes$diaant/lista.txt"
+sed -i "s/$/'&/" "$pasta/$ano$mes$diaant/lista.txt"
+sed -i "s/^/'/" "$pasta/$ano$mes$diaant/lista.txt"
 
-rm -rf "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_sinvp-015"
-rm -rf "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista0.txt"
-rm -rf "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista00.txt"
-rm -rf "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/lista1.txt"
-rm -rf "/home/isaac/ISAAC - DOCS/IMPRENSA/NGINX/$ano$mes$diaant/ips1.txt"
-sh robots.sh
+rm -rf "$pasta/$ano$mes$diaant/access_portal_log-"$ano""$mes""$diaant"_sinvp-015"
+rm -rf "$pasta/$ano$mes$diaant/lista0.txt"
+rm -rf "$pasta/$ano$mes$diaant/lista00.txt"
+rm -rf "$pasta/$ano$mes$diaant/lista1.txt"
+rm -rf "$pasta/$ano$mes$diaant/ips1.txt"
+sh "$pasta"/robots.sh
